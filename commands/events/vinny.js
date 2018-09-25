7f67b155b9a4bacab2ecabf1e94ae65fcdc7ce8b
Emmett Lin <emmett@linbarbell.com>
2018-09-24 22:33:13 -0400

    add parameters to event commands

--- commands/events/vinny.js
+++ commands/events/vinny.js
@@ -9,7 +9,19 @@ class VinnyCommand extends commando.Command
             name: 'vinny',
             group: 'events',
             memberName: 'vinny',
-            description: 'Vindicta event details'
+            description: 'Vindicta event details',
+            args: [
+                {
+                    key: 'date',
+                    prompt: 'What date is the event? (e.g. Friday 28 September)',
+                    type: 'string'
+                },
+                {
+                    key: 'time',
+                    prompt: 'What time is the event? (e.g. 21:00)',
+                    type: 'string'
+                }
+            ]
         });
     }
 
@@ -20,7 +32,7 @@ class VinnyCommand extends commando.Command
         .setColor(0x00AE86)
         .setFooter("Please remember that this is completely for fun! \n Please get your kill count before the event start time, you keep your kc even though you leave GWD2!\n Keep moving! Don't stand in the flames! \n Thats all the advice you need but if you are nervous, please feel free to PM me.\n All loot will be traded to myself and split equally between attendees.", "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/facebook/138/european-castle_1f3f0.png")
         .setThumbnail("https://vignette.wikia.nocookie.net/runescape2/images/4/40/Gorvek.png/revision/latest/scale-to-width-down/200?cb=20170517031043")
-        .addField("\u200b","📅 **Date:** Tuesday 3rd July\n 🕘 **Time:** 21:00 game-time\n🌍 **World:** 23\n**Host:** <@258094952137883648>")
+        .addField("\u200b","📅 **Date:** " + args.date + "\n 🕘 **Time:** " + args.time + " game-time\n🌍 **World:** 23\n**Host:** " + message.author)
         .addField("\u200b", "**Requirements:**\n 80 Attack\n40 Zaros kill count.", true)
         .addField("\u200b", "**Recommended:**\n 71+ Prayer(for peity)\n90+ Herb for extremes\n Anything above barrows armour\n Any style works \n Heart tele tabs \n BoB filled with food.", true)
         message.channel.sendEmbed(myInfo);
