9fa3d324c14c359bb3527b4d9ed8d60eaaaa1ebf
Emmett Lin <emmett@linbarbell.com>
2018-09-24 22:33:13 -0400

    add parameters to event commands

--- commands/events/capping.js
+++ commands/events/capping.js
@@ -9,7 +9,25 @@ class CappingCommand extends commando.Command
             name: 'capping',
             group: 'events',
             memberName: 'capping',
-            description: 'Capping event details'
+            description: 'Capping event details',
+            examples: ['.capping "28 September" "Runescape Gods" "For this weeks capping event, we ask you to wear the colours, outfits, or gear of any of your favourite runescape Gods! We can\'t wait to see what outfits & the variety of stuff you will all wear!"'],
+            args: [
+                {
+                    key: 'date',
+                    prompt: 'What date is the capping event? (e.g. 28 September)',
+                    type: 'string'
+                },
+                {
+                    key: 'theme',
+                    prompt: 'What theme is the capping event? (e.g. Runescape Gods)',
+                    type: 'string'
+                },
+                {
+                    key: 'description',
+                    prompt: 'Describe the event',
+                    type: 'string'
+                }
+            ]
         });
     }
 
@@ -20,8 +38,8 @@ class CappingCommand extends commando.Command
         .setColor(0x00AE86)
         .setFooter("When skilling with fellow clan members at the same time in the Citadel, the resource gain has been slightly increased, the more members you gather resources with at once, the better the gain!", "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/facebook/138/european-castle_1f3f0.png")
         .setThumbnail("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/129/european-castle_1f3f0.png")
-        .addField("\u200b","**Date:** Friday 14th September\n **Time:** 19:30 game-time\n **Host:** <@174725556011532289>\n **Theme:** Runescape Gods")
-        .addField("\u200b", "For this weeks capping event, we ask you to wear the colours, outfits, or gear of any of your favourite runescape Gods! We can't wait to see what outfits & the variety of stuff you will all wear!", true)
+        .addField("\u200b","**Date:** Friday " + args.date + "\n **Time:** 19:30 game-time\n **Host: **" + message.author +" \n **Theme: **" + args.theme)
+        .addField("\u200b", args.description, true)
         .addBlankField();
         message.channel.sendEmbed(myInfo);
     }
