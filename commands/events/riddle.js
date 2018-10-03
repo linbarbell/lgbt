fdb1901349b8b62b2fd689fabde888ffbb600219
ChristianArroyo1 <christian.arroyo@protonmail.com>
2018-10-03 15:50:07 -0500

    added riddle command

---
+++ commands/events/riddle.js
@@ -0,0 +1,47 @@
+const commando = require('discord.js-commando');
+const discord = require('discord.js');
+
+class RiddleCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'riddle',
+            group: 'events',
+            memberName: 'riddle',
+            description: 'riddle event details',
+            examples: ['.riddle "28 September" "20:00" "Describe where to meet and a basic premise here"'],
+            args: [
+                {
+                    key: 'date',
+                    prompt: 'What date is the capping event? (e.g. 28 September)',
+                    type: 'string'
+                },
+                {
+                    key:'time',
+                    prompt: 'What time is the event at? (In game time)',
+                    type: 'string'
+                },
+                {
+                    key: 'description',
+                    prompt: 'Describe the event',
+                    type: 'string'
+                }
+            ]
+        });
+    }
+
+    async run(message, args)
+    {
+        var myInfo = new discord.RichEmbed()
+        .setTitle("🏰 __**RIDDLE AND SEEK**__ 🏰")
+        .setColor(0x4F2095)
+        .setThumbnail("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/146/world-map_1f5fa.png")
+        .addField("\u200b","**Date:** Friday " + args.date + "\n **Time:** " + args.time + "\n **Host: **" + message.author)
+        .addField("\u200b", args.description, true)
+        .addBlankField();
+        message.channel.sendEmbed(myInfo);
+    }
+}
+
+module.exports = RiddleCommand;
