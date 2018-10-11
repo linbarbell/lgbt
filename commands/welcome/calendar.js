f1743a4fb47aacdbfe36777665e76c0e404d3d7d
Emmett Lin <emmett@linbarbell.com>
2018-10-10 22:11:52 -0400

    - update calendar to oct - update rbheart - add skilling event

--- commands/welcome/calendar.js
+++ commands/welcome/calendar.js
@@ -19,7 +19,7 @@ class CalendarCommand extends commando.Command
         .setColor(0xFD00FF)
         .setFooter("(remember to check back frequently for upcoming events and updates)")
         .addField("🏅 __**EVENTS CALENDAR**__ 🏅", "Being a community clan, we try to plan and host frequent events for clannies to participate in and socialise. Please find our current & most up-to-date events calendar below.\n \nIf you wish to host an event yourself please let a member of the Admin or Leadership team know and we will add it to the calendar and create a broadcast message.\n \n[Click here to view our calendar](https://docs.google.com/spreadsheets/d/1voAMbOib3-6r0wB_e7eVTS2mWM-lFXSTujLaKdhh8o4/edit?usp=sharing)")
-        .setImage("https://i.imgur.com/J8DHznr.png");
+        .setImage("https://i.gyazo.com/21aa004644cdde7d2e72c2540957a4dc.png");
         message.channel.sendEmbed(myInfo);
     }
 }
--- commands/welcome/welcome.js
+++ commands/welcome/welcome.js
@@ -16,7 +16,7 @@ class WelcomeInfoCommand extends commando.Command
     async run(message, args)
     {
         var myInfo = new discord.RichEmbed()
-        .setTitle("<:rbheart:304232552435023872> __**WELCOME TO THE OFFICIAL LGBT+ CORNER DISCORD SERVER**__ <:rbheart:304232552435023872>")
+        .setTitle("<:rbheart:495103097547259925> __**WELCOME TO THE OFFICIAL LGBT+ CORNER DISCORD SERVER**__ <:rbheart:495103097547259925>")
         .setColor(0xFF0000)
         .setDescription("A place for LGBT+ Runescapers from across the globe to get together, play games, chat, have a laugh and make friends! Below you'll find some handy information about our Clan, the Citadel and our Discord Channel.\n \n🌍**Official home world: 23**🌍")
         .setFooter("Last updated")
