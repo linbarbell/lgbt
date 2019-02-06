ae474eab876efef7db97796dac4689840befa7fc
ChristianArroyo1 <43612599+ChristianArroyo1@users.noreply.github.com>
2019-02-05 18:13:48 -0600

    add hardkril command (#19)

--- commands/events/hardkril.js
+++ commands/events/hardkril.js
@@ -1,15 +1,15 @@
 const commando = require('discord.js-commando');
 const discord = require('discord.js');
 
-class HardgraarCommand extends commando.Command
+class HardkrilCommand extends commando.Command
 {
     constructor(client)
     {
         super(client,{
-            name: 'hardgraar',
+            name: 'hardkril',
             group: 'events',
-            memberName: 'hardgraar',
-            description: 'Hard Mode Graardor event details',
+            memberName: 'hardkril',
+            description: 'Hard Mode Kril event details',
             args: [
                 {
                     key: 'date',
@@ -29,16 +29,16 @@ class HardgraarCommand extends commando.Command
     {
         message.delete();
         var myInfo = new discord.RichEmbed()
-        .setTitle("⚔️ __**HARDMODE Graardor MASS**__ ⚔️")
+        .setTitle("⚔️ __**HARDMODE K'ril MASS**__ ⚔️")
         .setColor(0x00AE86)
-        .setFooter("Please remember that this is completely for fun! Just hit Graardor and avoid the specials. That's all the advice you need but if you are nervous, please feel free to PM me. All loot will be traded to myself and split equally between attendees.", "https://runescape.wiki/images/thumb/b/b8/General_Graardor.png/375px-General_Graardor.png?3b673")
-        .setThumbnail("https://runescape.wiki/images/thumb/b/b8/General_Graardor.png/375px-General_Graardor.png?3b673")
+        .setFooter("Please remember that this is completely for fun! Just hit K'ril and avoid the specials. That's all the advice you need but if you are nervous, please feel free to PM me. All loot will be traded to myself and split equally between attendees.", "https://runescape.wiki/images/2/2f/K%27ril_Tsutsaroth.png")
+        .setThumbnail("https://runescape.wiki/images/2/2f/K%27ril_Tsutsaroth.png")
         .addField("\u200b","📅 **Date:** " + args.date + "\n🕘 **Time:** " + args.time + " game-time\n🌍 **World:** 23\n**Host:** " + message.author)
-        .addField("\u200b", "[Hard mode strategies for General Graardor](https://runescape.wiki/w/General_Graardor/Strategies/Hard_mode)")
-        .addField("\u200b", "**Requirements:**\nTroll Stronghold \n70 Strength", true)
+        .addField("\u200b", "[Hard mode strategies for K'ril Tsutsaroth](https://runescape.wiki/w/K%27ril_Tsutsaroth/Strategies/Hard_mode)")
+        .addField("\u200b", "**Requirements:**\nTroll Stronghold \n70 Constutution", true)
         .addField("\u200b", "**Recommended:**\nTier 70 Armour or higher \n Overloads \n Soulsplit", true)
         message.channel.sendEmbed(myInfo);
     }
 }
 
-module.exports = HardgraarCommand;
+module.exports = HardkrilCommand;
