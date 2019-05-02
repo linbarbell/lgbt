78506ffc6223f3b83519a85d3ec9b199c65ac60f
Emmett Lin <emmett@linbarbell.com>
2019-05-01 21:53:39 -0400

    add image resource link

--- commands/admin/admin-links.js
+++ commands/admin/admin-links.js
@@ -19,7 +19,8 @@ class AdminLinksCommand extends commando.Command
         .setColor(0x991919)
         .addField("🔗 __**HELPFUL LINKS**__ 🔗", 
         "[Click here to visit the forum thread](http://services.runescape.com/m=forum/c=w9VNiocRUOQ/sl=0/forums.ws?290,291,31,66034838)\n\n"+
-        "[Click here to view the warning spreadsheet](https://docs.google.com/spreadsheets/d/19y9dEi98kO0loHWhzZpe0a-fPSa1CXZ8b4p_PYC7L7g)")
+        "[Click here to view the warning spreadsheet](https://docs.google.com/spreadsheets/d/19y9dEi98kO0loHWhzZpe0a-fPSa1CXZ8b4p_PYC7L7g)\n\n"+
+        "[Click here for image resources](https://drive.google.com/drive/folders/1DcAYqV-PNgKpVonMWEsV4SpnFvjRDeJw)")
         .setThumbnail("https://www.smashbros.com/wiiu-3ds/images/character/toon_link/main.png")
         .setFooter("Last updated")
         .setTimestamp();
